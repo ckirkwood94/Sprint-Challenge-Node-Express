c3b93333eb39cfd96f62e5af40d0f57823ea9d0f
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-05 11:55:37 -0400

    Router files created

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const configureMiddleware = require('./config/middleware.js');
+const actionRoutes = require('./routes/actionRoutes');
+const projectRoutes = require('./routes/projectRoutes');
 
 const port = 9000;
 
 const server = express();
 
-// middleware
+// MIDDLEWARE
 configureMiddleware(server);
+
+// ROUTES
+server.use('/api/actions', actionRoutes);
+server.use('/api/projects', projectRoutes);
 
 server.listen(port, () => {
   console.log(`\n=== API running on port ${port} ===\n`);
