17612fcc6d5ff17927c5c6978249289a07afb2e3
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-10-05 13:45:30 -0400

    Modified middleware.js. Moved middleware from index.js, and added other middleware

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const logger = require('morgan');
+const cors = require('cors');
 
 module.exports = (server) => {
   server.use(helmet());
   server.use(express.json());
+  server.use(logger('tiny'));
+  server.use(cors());
 };
